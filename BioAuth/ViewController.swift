3cb71e7522e5f213fc00d8ba65b766da2b1e6e7d
Bhumika Patel <bhumikapatel251@gmail.com>
2022-03-30 14:45:29 +0530

    first commit

---
+++ BioAuth/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  BioAuth
+//
+//  Created by Bear Cahill on 7/8/19.
+//  Copyright © 2019 Bear Cahill. All rights reserved.
+//
+
+import UIKit
+import LocalAuthentication
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        view.addSubview(button)
+        button.center = view.center
+        button.setTitle("Authentication", for: .normal)
+        button.backgroundColor = .systemGreen
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+    
+    @objc func didTapButton() {
+        let context = LAContext()
+        
+        var error: NSError? = nil
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            
+            let reason = "please authorize with touch id!"
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
+                [weak self] success, error in
+                DispatchQueue.main.async {
+                    guard success, error == nil else {
+                    let alert = UIAlertController(title: "Failed Authentication", message: "please try again.", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+                        self?.present(alert, animated: true)
+                    
+                    return //failed
+                        
+                }
+                    //show other screen
+                    let vc = UIViewController()
+                    vc.title = "Welcome!"
+                    vc.view.backgroundColor = .systemBlue
+                    self?.present(UINavigationController(rootViewController: vc), animated: true, completion: nil)
+                
+                }
+         
+            }
+    
+        }
+          
+        else{
+                let alert = UIAlertController(title: "Unavailable", message: "Please enable FaceID", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+                present(alert, animated: true)
+        
+        }
+    }
+}
+
